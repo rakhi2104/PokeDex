8ff681c40100103b65fa3f1475de62f0d2f1fd7a
Rakesh Peela <rakhi2104@gmail.com>
2021-05-10 01:35:32 +0530

    fix lint issues and new dist/index.js for action

--- src/App.js
+++ src/App.js
@@ -32,6 +32,7 @@ function App() {
       setAppState({ ...appState, loading: false });
     }
     init();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [filters]);
 
   return (
