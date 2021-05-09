f0f04820ef40406e3c164302205e9f9a690b8d63
Rakesh Peela <rakhi2104@gmail.com>
2021-05-10 00:24:32 +0530

    add compiled index.js

--- .github/actions/ping-slack/dist/index.js
+++ .github/actions/ping-slack/dist/index.js
@@ -9902,11 +9902,6 @@ const { default: axios } = __nccwpck_require__(8211);
 const sample_payload = __nccwpck_require__(5509);
 
 try {
-  //   const webhookUrl = core.getInput("slack-webhook-url");
-  const event = core.getInput("type");
-  const time = new Date().toTimeString();
-  core.setOutput("time", time);
-
   var payload = JSON.stringify(github.context.payload, undefined, 2);
   if (payload === {}) {
     payload = sample_payload;
@@ -9915,7 +9910,7 @@ try {
     ref,
     repository: { html_url: repoUrl, full_name: repoName },
     sender: { login: authorName, url: senderUrl, avatar_url: senderAvatarUrl },
-  } = sample_payload;
+  } = payload;
   core.info("Creating payload ...");
   const slackMessagePayload = getSlackPayload({
     repoName,
@@ -9926,7 +9921,6 @@ try {
     senderUrl,
   });
   core.info("Sending Message ...");
-  console.log("eNv", process.env.SLACK_WEBHOOK_URL);
 
   // Post Message
   axios
